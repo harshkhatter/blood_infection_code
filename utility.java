9792a5daabf8f35c9ef36b7d34686ac793fb9d71
Harsh Khatter <65993521+harshkhatter@users.noreply.github.com>
2022-07-14 12:52:20 +0530

    Create utility.java

---
+++ utility.java
@@ -0,0 +1,21 @@
+package com.gunjan.medihelp.database.util;
+
+public class Utility {
+    public static final String MALE = "male";
+    public static final String FEMALE = "female";
+    public static final String OTHERS = "others";
+    public static final String HB = "hb";
+    public static final String RBC = "rbc";
+    public static final String PC = "pc";
+    public static final String HCT = "hct";
+    public static final String MCV = "mcv";
+    public static final String MCH = "mch";
+    public static final String MCHC = "mchc";
+    public static final String RDW_CV = "rdwcv";
+    public static final String BS = "bs";
+    public static final String VERY_LOW = "very low";
+    public static final String LOW = "low";
+    public static final String NORMAL = "normal";
+    public static final String HIGH = "high";
+    public static final String VERY_HIGH = "very high";
+}
